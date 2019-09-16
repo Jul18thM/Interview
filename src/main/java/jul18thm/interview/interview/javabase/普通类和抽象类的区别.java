ceae5499f49c1b77a8d09ab722866b5f65c90ada
Jul18thM <jul.18th.m@gmail.com>
2019-09-16 09:52:23 +0800

    普通类和抽象类有哪些区别？

---
+++ src/main/java/jul18thm/interview/interview/javabase/普通类和抽象类的区别.java
@@ -0,0 +1,15 @@
+package jul18thm.interview.interview.javabase;
+
+/**
+ * @author Jul.18th.M
+ * @date 2019-09-16 9:41
+ * description 普通类和抽象类有哪些区别？
+ */
+public class 普通类和抽象类的区别 {
+
+    /*
+        1.普通类可以被实例化，抽象类不可以被实例化
+        2.普通类和抽象类都可以被继承，但是子类继承抽象类需要对抽象类中的抽象方法进行重写，否则子类也是抽象类
+     */
+
+}
