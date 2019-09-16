7e57cf03d0fdc7ac7039dff87f603e840527c8d0
Jul18thM <jul.18th.m@gmail.com>
2019-09-16 15:12:38 +0800

    java 中 IO 流分为几种？

---
+++ src/main/java/jul18thm/interview/interview/javabase/java中IO流分类.java
@@ -0,0 +1,16 @@
+package jul18thm.interview.interview.javabase;
+
+/**
+ * @author Jul.18th.M
+ * @date 2019-09-16 11:29
+ * description java 中 IO 流分为几种？
+ */
+public class java中IO流分类 {
+
+    /*
+     IO流的分类：
+     1.按照流的流向分，可以分为输入流和输出流
+     2.按照流的角色划分，可以分为节点流和处理流
+     3.按照流的操作单元分，可以分为字节流和字符流*/
+
+}
