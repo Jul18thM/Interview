c030fb4fb030170cc403324b9d2b606ee7c103fe
Jul18thM <jul.18th.m@gmail.com>
2019-09-19 13:51:44 +0800

    哪些集合类是线程安全的？

---
+++ src/main/java/jul18thm/interview/interview/container/线程安全的集合类.java
@@ -0,0 +1,18 @@
+package jul18thm.interview.interview.container;
+
+/**
+ * @author Jul.18th.M
+ * @date 2019-09-19 13:50
+ * description 哪些集合类是线程安全的？
+ */
+public class 线程安全的集合类 {
+
+    /**
+     * 一：Vector
+     */
+
+
+    /**
+     * 二：HashTable
+     */
+}
